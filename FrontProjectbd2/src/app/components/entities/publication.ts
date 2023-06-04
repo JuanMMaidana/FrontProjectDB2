a25f194a35ce78bb59aaeef2184ec8d696bf7d04
JuanMMaidana <juan.maidana@correo.ucu.edu.uy>
2023-06-04 00:59:08 -0300

    card v1

---
+++ FrontProjectbd2/src/app/components/entities/publication.ts
@@ -0,0 +1,11 @@
+export interface Publication {
+    id_publication: number;
+    title: string;
+    description: string;
+    date: string;
+    type_publication: boolean;
+    categories: string;
+    habilities: string;
+    photos: string;
+    state: string;
+}
--- FrontProjectbd2/src/app/components/publication/publication.component.ts
+++ FrontProjectbd2/src/app/components/publication/publication.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Publication } from '../entities/publication';
 
 @Component({
   selector: 'app-publication',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class PublicationComponent {
 
-}
+  publication: Publication = {
+    id_publication : 1, title : 'Árbol caido', description : 'Buenas, la semana pasada hubo una tormenta y desafortunadamente cayo un rayo en el barrio y hizo que un arbol este en el medio de la calle, necesito ayuda para que alguien me ayude a sacarlo, muchas gracias!', date :'2020-01-01', type_publication : true, categories : 'Personal', habilities :'Fueza', photos : '../../../assets/images/arbol2.jpg', state : 'Activo'}
+  }
+
+
