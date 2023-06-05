5fd383ea00ba861a9d2c4758669e1405590a2b75
JuanMMaidana <juan.maidana@correo.ucu.edu.uy>
2023-06-04 22:35:52 -0300

    card v2 + button v1 -> start services

--- FrontProjectbd2/src/app/app.module.ts
+++ FrontProjectbd2/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PublicationComponent } from './components/publication/publication.component';
+import { PubishButtonComponent } from './components/pubish-button/pubish-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PublicationComponent
+    PublicationComponent,
+    PubishButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ FrontProjectbd2/src/app/components/pubish-button/pubish-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pubish-button',
+  templateUrl: './pubish-button.component.html',
+  styleUrls: ['./pubish-button.component.css']
+})
+export class PubishButtonComponent {
+
+}
