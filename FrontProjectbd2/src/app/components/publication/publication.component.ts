169b207ef30d8c8471e9d6bf7ff779f9ec3eb8be
JuanMMaidana <juan.maidana@correo.ucu.edu.uy>
2023-06-25 21:42:59 -0300

    fechav2

--- FrontProjectbd2/src/app/components/publication/publication.component.ts
+++ FrontProjectbd2/src/app/components/publication/publication.component.ts
@@ -1,28 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 import { Publication } from '../entities/publication';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { ModalContactComponent } from '../modal-contact/modal-contact.component';
-
-
 
 @Component({
   selector: 'app-publication',
   templateUrl: './publication.component.html',
   styleUrls: ['./publication.component.css']
 })
-export class PublicationComponent {
-
+export class PublicationComponent implements AfterViewInit {
   constructor(private modalService: NgbModal) { }
 
-
   @Input() publication?: Publication;
+  public fechaFormateada: string | undefined;
 
+  ngAfterViewInit() {
+    this.formatFecha();
+  }
 
-  openModal(){
-    const modalRef = this.modalService.open(ModalContactComponent, {centered: true, size: 'md', animation: true});
+  async openModal() {
+    const modalRef = this.modalService.open(ModalContactComponent, { centered: true, size: 'md', animation: true });
     modalRef.componentInstance.publication = this.publication;
   }
 
+  private formatFecha() {
+    if (this.publication) {
+      const fecha = new Date(this.publication.fecha);
+      this.fechaFormateada = fecha.toLocaleDateString();
+    }
   }
-
-
+}
